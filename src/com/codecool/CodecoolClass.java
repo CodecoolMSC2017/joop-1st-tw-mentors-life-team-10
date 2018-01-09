138de6755cdffde8ffcb825872c8fd120db1bcca
Tamas Kiss <sauriaster@gmail.com>
2018-01-09 15:08:30 +0100

    package temporary deleted

--- src/com/codecool/CodecoolClass.java
+++ src/com/codecool/CodecoolClass.java
@@ -1,15 +1,14 @@
-package com.codecool;
+
 
 import java.util.Scanner;
 import java.util.Arrays;
 import java.lang.Integer;
-import java.nio.channels.FileLockInterruptionException;
 import java.io.*;
 
 public class CodecoolClass {
     public String location;
     int year;
-    Mentor[] mentors = new Mentors[0];
+    Mentor[] mentors = new Mentor[0];
     //Student[] students = new Student[0];
     private final int EXTEND_WITH_ONE = 1;
     private final int ONE_FOR_LAST_ITEM = 1;
@@ -21,28 +20,25 @@ public class CodecoolClass {
         this.year = year;
         try{
             File openMentor = new File(mentorsCsvPath);
-            Scanner read = new Scanner(openMentor).useDelimiter(",");
+            Scanner read = new Scanner(openMentor);
             while (read.hasNext()){
                 
                 String data = read.nextLine();
                 String[] tempArray = data.split(";");
+                for (String i: tempArray){
+                    System.out.println(i);
+                }
                 int age = Integer.parseInt(tempArray[2]);
                 int energyLevel = Integer.parseInt(tempArray[4]);
-                mentors = Arrays.copyOf(mentors, mentors.length + EXTEND_WITH_ONE);
-                mentors[mentors.length - ONE_FOR_LAST_ITEM] = new Mentor(tempArray[0], tempArray[1], age, tempArray[3], energyLevel, tempArray[5]);
+                mentors = Arrays.copyOf(mentors, mentors.length + 1);
+                mentors[mentors.length - 1] = new Mentor(tempArray[0], tempArray[1], age, tempArray[3], energyLevel, tempArray[5]);
                 System.out.format("%d %d %s\n", age, energyLevel, tempArray[0]);            
             }
             read.close();
-        }
-        catch (FileLockInterruptionException expt){
-            System.out.println("No such file found");
-        }
-
-        
-    }
-
-    public static CodecoolClass createLocal(){
-
+            }
+            catch (FileNotFoundException expt){
+                System.out.println("No such file found");
+            }        
     }
 
     public static void main(String[] args){
