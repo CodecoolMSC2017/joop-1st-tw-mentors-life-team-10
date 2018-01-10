1cfd106322940c9eb43e7a29e4ebba4df3c56eee
Tamas Kiss <sauriaster@gmail.com>
2018-01-10 11:32:54 +0100

    Student: knowledgeLevel now taken as argument

--- src/com/codecool/Student.java
+++ src/com/codecool/Student.java
@@ -1,15 +1,14 @@
 package com.codecool;
 
 public class Student extends Person {
-
     int knowledgeLevel;
 
     Student(String firstName, String lastName, int yearOfBirth, String gender, int energyLevel, int knowledgeLevel){ 
     super(firstName, lastName, yearOfBirth, gender, energyLevel);
-    knowledgeLevel = 100;
+    this.knowledgeLevel = knowledgeLevel;
     }
 
-    int getKnowledgeLevel(){
+    public int getKnowledgeLevel(){
         return knowledgeLevel;
     }
 }
