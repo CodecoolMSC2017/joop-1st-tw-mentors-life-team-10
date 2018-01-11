95ac4cc064763ca1faef00f0fe3443088a1f11d2
ambrusBence1 <o.g.bence@gmail.com>
2018-01-11 13:32:16 +0100

    modificate Kitchen.java constructors array option

--- src/com/codecool/Kitchen.java
+++ src/com/codecool/Kitchen.java
@@ -8,15 +8,17 @@ public class Kitchen{
     boolean clean;
 
     Kitchen(){
-        food = new String[3];
-        food[0] = "raw fish";
-        food[1] = "rotten eggs";
-        food[2] = "barely dead pig";
+        food = new String[4];
+        food[0] = "Raw fish";
+        food[1] = "Rotten eggs";
+        food[2] = "Barely dead pig";
+        food[3] = "Half baked squirrel";
 
-        foodFrom = new String[3];
-        foodFrom[0] = "dustbin";
-        foodFrom[1] = "depths of the refigrator";
-        foodFrom[2] = "under the table";
+        foodFrom = new String[4];
+        foodFrom[0] = "the dustbin.";
+        foodFrom[1] = "the dark side of the refigrator.";
+        foodFrom[2] = "under the table.";
+        foodFrom[3] = "Lindi Imres secret food hide-out."
 
         clean = true;
     }
@@ -35,8 +37,8 @@ public class Kitchen{
 
     void feedThePerson(){
         Random rand = new Random();
-        int n = rand.nextInt(3) + 1;
-        int m = rand.nextInt(3) + 1;
+        int n = rand.nextInt(4) + 1;
+        int m = rand.nextInt(4) + 1;
         System.out.println(food[n-1] + " from " + foodFrom[m-1]);
     }
 
